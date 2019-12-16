9019fb38f432f91e65746b07f3d0db9f683e64e7
wangpeiqi <LogicWang@outlook.com>
2019-12-16 23:43:18 +0800

    add primer solution

--- src/primer.py
+++ src/primer.py
@@ -5,8 +5,35 @@
 input a number n
 output list of all primers less than n
 """
+import time
+import math
 
 
-def is_primer(n):
-    pass
+def is_primer(x):
+    sqrt_x = int(math.sqrt(x))
+    for i in range(2, sqrt_x):
+        if x % i == 0:
+            return False
+    return True
 
+
+def traversal(n):
+    p_list = [2]
+    """
+    遍历法
+    :param n:
+    :return:
+    """
+    for x in range(3, n):
+        if is_primer(x):
+            # print(x)
+            p_list.append(x)
+    return p_list
+
+
+if __name__ == '__main__':
+    start = time.time()
+    res = traversal(1000000)
+    print(len(res))
+    print(res[-20:])
+    print("time consume ", time.time() - start)
