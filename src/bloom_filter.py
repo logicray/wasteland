c8277aa54e34b002c267252c67377057415e5970
wangpeiqi <LogicWang@outlook.com>
2019-12-26 23:37:17 +0800

    1

---
+++ src/bloom_filter.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+# -*- coding:utf8 -*-
+
+"""
+simple implement of bloom filter
+"""
+
+print(__doc__)
+
+
+class BloomFilter:
+    def __init__(self, size):
+        self.size = size
+        self.bit_array = [False] * size
+
+    def hash1(self, n):
+        return hash(n) % self.size
+
+    def hash2(self, n):
+        return hash(str(n)+str(n)) % self.size
+
+    def push(self, n):
+        tmp_index1 = self.hash1(n)
+        self.bit_array[tmp_index1] = True
+
+        tmp_index2 = self.hash2(n)
+        self.bit_array[tmp_index2] = True
+
+    def is_exist(self, n):
+        bit_index1 = self.hash1(n)
+        bit_index2 = self.hash2(n)
+        if self.bit_array[bit_index1] and self.bit_array[bit_index2]:
+            return True
+        return False
+
+
+if __name__ == '__main__':
+    b_filter = BloomFilter(100)
+    print(len(b_filter.bit_array))
+    b_filter.push("a")
+    b_filter.push("b")
+    exist = b_filter.is_exist("b")
+    print(exist)
--- src/primer.py
+++ src/primer.py
@@ -7,10 +7,12 @@ output list of all primers less than n
 """
 import time
 import math
+import numpy as np
+from collections import deque
 
 
 def is_primer(x):
-    sqrt_x = int(math.sqrt(x))
+    sqrt_x = int(math.sqrt(x))+1
     for i in range(2, sqrt_x):
         if x % i == 0:
             return False
@@ -31,7 +33,7 @@ def traversal(n):
     return p_list
 
 
-def sieve_era(n):
+def sieve_erato(n):
     """
     埃氏筛法(Sieve of Eratosthenes, SoE)
     :param n:
@@ -54,41 +56,42 @@ def sieve_euler(n):
     :param n:
     :return:
     """
-    p = [True] * (n + 1)
+    prime_list = deque()
+    p = [True] * (n + 1)  # true 表示是素数
     p[0] = p[1] = False
-    for i in range(2, int(math.sqrt(n) + 1)):
+    for i in range(2, n):
         if p[i]:
-            for j in range(i * i, n + 1, i):
-                p[j] = False
-    prime = [i for i in range(n + 1) if p[i]]
-    print(len(prime))
+            # prime_list.append(i)
+            prime_list.append(i)
+        for j in prime_list:
+            t = i * j
+            if t > n:
+                break
+            p[t] = False
+            # 每个合数只被他的最小质因数筛去
+            if i % j == 0:
+                break
+
+    # prime = [i for i in range(n + 1) if p[i]]
+    print(len(prime_list))
+    # print(prime)
+    # print(prime_list)
+    return prime_list
 
 
-
-
-    return prime
-
-
-def sieve_linear(n):
-    """
-    欧拉筛法(Sieve of Euler)
-    :param n:
-    :return:
-    """
-    p = [True] * (n + 1)
-    p[0] = p[1] = False
-    for i in range(2, int(math.sqrt(n) + 1)):
-        if p[i]:
-            for j in range(i * i, n + 1, i):
-                p[j] = False
-    prime = [i for i in range(n + 1) if p[i]]
-    print(len(prime))
-    return prime
+def check_equal(n):
+    traversal_res = traversal(n)
+    sieve_erato_res = sieve_euler(n)
+    # print(traversal_res, sieve_erato_res)
+    if traversal_res == sieve_erato_res:
+        return True
+    return False
 
 
 if __name__ == '__main__':
     start = time.time()
-    res = traversal(2000000)
+    res = sieve_euler(2000000)
     print(len(res))
-    print(res[-20:])
+    # print(res[-20:])
     print("time consume ", time.time() - start)
+    # print(check_equal(40))
